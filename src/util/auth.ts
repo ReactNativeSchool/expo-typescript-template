3a13a80196e1f8e7a677043e2e3673d34ae5bf23
Spencer Carli <spencercarli@gmail.com>
2021-06-01 10:23:56 -0500

    Convert constants and util

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import { Alert } from 'react-native';
+
+type ErrorType = {
+  email?: string;
+  password?: string;
+};
 
 export const useLogin = () => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
-  const [errors, setErrors] = React.useState({});
+  const [errors, setErrors]: [
+    ErrorType,
+    Dispatch<SetStateAction<{}>>,
+  ] = React.useState({});
 
   const submit = () => {
-    const nextErrors = {};
+    const nextErrors: ErrorType = {};
     if (email.length === 0) {
       nextErrors.email = 'This field is required.';
     }
